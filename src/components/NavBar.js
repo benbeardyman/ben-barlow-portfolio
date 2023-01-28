39b838f507b7bc36cfe1c86cc44f5e73ae644ff6
Ben Barlow <ben.barlow@yahoo.co.uk>
2023-01-28 09:53:20 +0000

    nav sidebar fixed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,25 @@
+import styled from "styled-components"
+
+const Wrapper = styled.nav`
+    height: 100vh;
+    background-color: #abd1c6;
+`
+const Links = styled.ul`
+    margin:0;
+    list-style: none;
+`
 
 
 const NavBar = () => {
 
     return (
-        <ul>
-            <li>
-                About
-            </li>
-            <li>
-                Projects
-            </li>
-            <li>
-                Contact
-            </li>
-        </ul>
+        <Wrapper>
+            <Links>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </Links>
+        </Wrapper>
     )
 
 }
