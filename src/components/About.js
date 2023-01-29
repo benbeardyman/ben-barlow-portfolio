beccb93e518d5c9aff208b7c6e04e3efff2db34a
Ben Barlow <ben.barlow@yahoo.co.uk>
2023-01-29 21:06:12 +0000

    component updates

--- src/components/About.js
+++ src/components/About.js
@@ -1,28 +1,44 @@
 import styled from "styled-components"
 import pixelMug from "../images/pixel_mug.jpg"
 
-
-
-const Wrapper = styled.section`
+const Section = styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: center;
     height: 100vh;
     background-color: #004643;
 `
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-around;
+    height: 60%;
+    width: 80%;
+    background-color: #e8e4e6;
+    border:5px solid;
+    border-radius: 15px;
+    border-color: #001e1d;
+`
 
 const Header = styled.h2`
-    color: #fffffe;
+    color: #001e1d;
 `
 
 const ProfileImage = styled.img`
-    border-radius: 30%;
+    margin: 20px;
+    border:5px solid;
+    border-radius: 15px;
+    border-color: #001e1d;
 `
 
 const About = () => {
 
     return (
-        <Wrapper id="about">
-            <Header>About</Header>
-            <ProfileImage src={pixelMug} height="200" />
-        </Wrapper>
+        <Section id="about">
+            <Wrapper>
+                <Header>About</Header>
+                <ProfileImage src={pixelMug} height="200" />
+            </Wrapper>
+        </Section>
     )
 
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,35 @@
 import styled from "styled-components"
 
-const Wrapper = styled.section`
+const Section = styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: center;
     height: 100vh;
     background-color: #004643;
 `
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-around;
+    height: 60%;
+    width: 80%;
+    background-color: #e8e4e6;
+    border:5px solid;
+    border-radius: 15px;
+    border-color: #001e1d;
+`
 
 const Header = styled.h2`
-    color: #fffffe;
+    color: #001e1d;
 `
 
 const Contact = () => {
 
     return (
-        <Wrapper id="contact">
-            <Header>Contact</Header>
-        </Wrapper>
+        <Section id="contact">
+            <Wrapper>
+                <Header>Contact</Header>
+            </Wrapper>
+        </Section>
     )
 
 }
