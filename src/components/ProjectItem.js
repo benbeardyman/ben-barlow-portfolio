65970b8b49a94730aff973d22fde054d7b8a50fd
Ben Barlow <ben.barlow@yahoo.co.uk>
2023-02-10 10:41:15 +0000

    changes

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,17 +1,17 @@
 import styled from "styled-components"
 
-const ProjectItem = ({name, description, image, url}) => {
+const Wrapper = styled.div`
+display: flex;
+justify-content: space-around;
+height: 200px;
+width: 200px;
+background-color: #e8e4e6;
+border:5px solid;
+border-radius: 15px;
+border-color: #001e1d;
+`
 
-    const Wrapper = styled.div`
-        display: flex;
-        justify-content: space-around;
-        height: 200px;
-        width: 200px;
-        background-color: #e8e4e6;
-        border:5px solid;
-        border-radius: 15px;
-        border-color: #001e1d;
-    `
+const ProjectItem = ({name, description, image, url}) => {
 
     return (
         <Wrapper>
