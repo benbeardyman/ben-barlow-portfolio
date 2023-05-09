4db290937b634f4ce76b0d891caab4e3d41329ad
Ben Barlow <ben.barlow@yahoo.co.uk>
2023-05-09 17:22:55 +0100

    added solo app details

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,26 +1,25 @@
 const projectsData = [
     {
         id: 1,
-        name: "Around The World App",
-        description: "A full stack JavaScript country tracking app allowing the user to create a bucket list of countries they want to visit and track where they have already been.",
+        name: "Solo Travel App",
+        description: "A full stack mobile app built with React-Native and Java. The app is aimed at colo travellers enabling them to meet up with other solo travellers in various cities by creating and joining events.",
+        img: "solo_screenshot.jpg",
+        url: "https://github.com/benbeardyman/Solo_Travel_App",
+    },
+    {
+        id: 2,
+        name: "Around The World Bucket List App",
+        description: "A full stack React app allowing the user to create a bucket list of countries they want to visit and track where they have already been.",
         img: "around_the_world_app.jpg",
         url: "https://github.com/benbeardyman/Around_The_World_Travel_App",
     },
     {
-        id: 2,
+        id: 3,
         name: "Shop Inventory App",
         description: "CRUD web application that provides the ability to manage a camera shop inventory, adding products and manufacturers and editing stock levels. Created with Python using the Flask library, a PostgreSQL database and RESTful routes.",
         img: "shop_inv_app.jpg",
         url: "https://github.com/benbeardyman/Camera_Shop_Inventory_App",
     },
-    
-    // {
-    //     id: 3,
-    //     name: "placeholder 3",
-    //     description: "this is my third project",
-    //     img: "image",
-    //     url: "url",
-    // }
 ]
 
 export default projectsData
