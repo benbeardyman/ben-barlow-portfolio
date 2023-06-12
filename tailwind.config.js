d1c8e4dddeccd9bca3fdde652f1583d976c932f7
Ben Barlow <ben.barlow@yahoo.co.uk>
2023-06-12 11:47:25 +0100

    background to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      // backgroundImage: {
+      //   'tile':"url('src/images/bground_tile.jpg')",
+      // }
+    },
+  },
+  plugins: [],
+}
+
