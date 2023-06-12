a3c831f6f56fa4a2c4e7071d0329a433685de32b
Ben Barlow <ben.barlow@yahoo.co.uk>
2023-06-12 22:33:01 +0100

    horizontal scroll on projects

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,13 +1,15 @@
 const ProjectItem = ({ name, description, image, url }) => {
 
     return (
-        <div className="flex h-1/4 w-3/4 bg-clotted-cream border-4 border-coral rounded-xl">
-            <div className="flex flex-col justify-evenly items-start">
-                <h3 className="px-4 pt-4 m-0">{name}</h3>
-                <p className="px-4">{description}</p>
-                <a href={url} target="_blank" className="px-4 pb-4 no-underline font-semibold text-coral">GitHub</a>
+        <div className="flex justify-center w-screen">
+            <div className="flex w-11/12 h-11/12 bg-clotted-cream border-4 border-coral rounded-xl ">
+                <div className="flex flex-col justify-evenly items-start">
+                    <h3 className="px-4 pt-4 m-0">{name}</h3>
+                    <p className="px-4">{description}</p>
+                    <a href={url} target="_blank" className="px-4 pb-4 no-underline font-semibold text-coral">GitHub</a>
+                </div>
+                <img src={process.env.PUBLIC_URL + "/" + image} className="self-center max-h-full rounded-tr-lg rounded-br-lg"/>
             </div>
-            <img src={process.env.PUBLIC_URL + "/" + image} className="self-center max-h-full rounded-tr-lg rounded-br-lg"/>
         </div>
     )
 }
