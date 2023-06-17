0312ac166a616e3d226f75ac1ecc788820345884
Ben Barlow <ben.barlow@yahoo.co.uk>
2023-06-17 10:28:05 +0100

    nav-arrow added

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,6 +1,13 @@
+import { Icon } from '@iconify/react'
 
 
 const Title = () => {
+
+    const scrollDown = () => {
+        const aboutComponent = document.getElementById('about');
+        aboutComponent.scrollIntoView({ behavior: 'smooth' });
+    };
+    
 
     return (
         <div id="title" className="flex flex-col items-center justify-center min-h-screen">
@@ -12,9 +19,17 @@ const Title = () => {
                     Barlow
                 </h1>
             </div>
-            <h2 className="text-coral text-4xl text-center font-title animate-fade ">
+            <h2 className="text-coral text-4xl text-center font-title animate-fade">
                 Software Developer
             </h2>
+            <div className="absolute bottom-0 mb-8 " >
+            <Icon
+                    icon="simple-line-icons:arrow-down"
+                    onClick={scrollDown}
+                    color="#ff9c99"
+                    className="h-12 w-12 opacity-70 cursor-pointer hover:opacity-100 animate-fade" 
+                />
+                </div>
         </div>
     )
 }
