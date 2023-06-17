48d8990364c4879d11651299febd9c3c6f636f4b
Ben Barlow <ben.barlow@yahoo.co.uk>
2023-06-17 15:47:40 +0100

    general tidying

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,26 +6,26 @@ const ProjectItem = ({ name, description, image, url, technologies, index}) => {
     const technologiesArray = technologies.map((technology, index) => {
         if (technology.iconName === 'firebase') {
             return (
-                <div className="group flex flex-col justify-center items-center">
+                <div className='group flex flex-col justify-center items-center'>
                     <Icon
                         key={index}
                         icon={'logos:firebase'}
                         className="w-12 h-12 ml-4 mt-2"
                     />
-                    <span className="opacity-0 group-hover:opacity-100 absolute bg-white text-black text-sm py-1 px-2 rounded-md font-primary">
+                    <span className='opacity-0 group-hover:opacity-100 absolute bg-white text-black text-sm py-1 px-2 rounded-md font-primary'>
                         {technology.name}
                     </span>
                 </div>
             )
         } else {
             return (
-                <div className="group flex flex-col justify-center items-center">
+                <div className='group flex flex-col justify-center items-center'>
                     <Icon
                         key={index}
                         icon={'skill-icons:' + technology.iconName}
                         className="w-12 h-12 ml-4 mt-2"
                     />
-                    <span className="opacity-0 group-hover:opacity-100 absolute bg-white text-black text-sm py-1 px-2 rounded-md font-primary">
+                    <span className='opacity-0 group-hover:opacity-100 absolute bg-white text-black text-sm py-1 px-2 rounded-md font-primary'>
                         {technology.name}
                     </span>
                 </div>
@@ -34,24 +34,24 @@ const ProjectItem = ({ name, description, image, url, technologies, index}) => {
     })
 
     return (
-        <div className="flex justify-center w-screen snap-center shrink-0">
-            <div className="flex w-11/12 bg-clotted-cream border-4 border-coral rounded-xl justify-between">
-                <div className="flex flex-col justify-center items-start">
-                    <div className="flex flex-row items-center justify-center">
-                        <h3 className="px-4 pt-4 pb-6 font-header text-lg">{name}</h3>
-                        <a href={url} target="_blank" rel="noreferrer" className="pb-4">
+        <div className='flex justify-center w-screen snap-center shrink-0'>
+            <div className='flex w-11/12 bg-clotted-cream border-4 border-coral rounded-xl justify-between'>
+                <div className='flex flex-col justify-center items-start'>
+                    <div className='flex flex-row items-center justify-center'>
+                        <h3 className='px-4 pt-4 pb-6 font-header text-lg'>{name}</h3>
+                        <a href={url} target="_blank" rel="noreferrer" className='pb-4'>
                             <Icon
                             icon="bi:github"
-                            className="h-10 w-10"
+                            className='h-10 w-10'
                             />
                         </a>
                     </div>
-                    <p className="px-4 font-primary">{description}</p>
-                    <h4 className="px-4 pt-4 m-0 font-header">Tech:</h4>
-                    <div className="flex flex-wrap">{technologiesArray}</div>    
+                    <p className='px-4 font-primary'>{description}</p>
+                    <h4 className='px-4 pt-4 m-0 font-header'>Tech:</h4>
+                    <div className='flex flex-wrap'>{technologiesArray}</div>    
                 </div>
                 
-                <img src={process.env.PUBLIC_URL + "/" + image} className="self-center max-h-full rounded-tr-lg rounded-br-lg"/>
+                <img src={process.env.PUBLIC_URL + "/" + image} className='self-center max-h-full rounded-tr-lg rounded-br-lg'/>
             </div>
         </div>
     )
