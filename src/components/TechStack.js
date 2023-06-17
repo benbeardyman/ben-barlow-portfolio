7db97ce4c271ddc2d3e64e465d76deab14038a0b
Ben Barlow <ben.barlow@yahoo.co.uk>
2023-06-17 16:17:12 +0100

    tidying

--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -12,6 +12,9 @@ const TechStack = () => {
     return (
         <div id="tech_stack" className="flex flex-col items-center justify-center min-h-screen w-screen">
             <div className="flex flex-col items-center w-9/12 bg-clotted-cream border-4 border-coral rounded-3xl pt-8">
+                <h3 className="mb-2 text-center font-header text-xl">
+                    Tech Stack
+                </h3>
                 <div className="flex justify-center items-start">
                 <div className="flex flex-col px-6">
                     <h3 className={header}>Front-End</h3>
@@ -69,7 +72,26 @@ const TechStack = () => {
                         <Icon icon="skill-icons:expressjs-light" className="w-8 h-8 mr-2"/>
                         Express.js
                     </span>
+                </div>
+                
+                <div className="flex flex-col px-6">
+                    <h3 className={header}>Miscellaneous</h3>
                     
+                    <span className="flex items-center font-primary text-base pb-3">
+                        <Icon icon="skill-icons:postgresql-light" className="w-8 h-8 mr-2"/>
+                        PostgreSQL
+                    </span>
+
+                    <span className="flex items-center font-primary text-base pb-3">
+                        <Icon icon="skill-icons:mongodb" className="w-8 h-8 mr-2"/>
+                        MongoDB
+                    </span>
+                        
+                    <span className="flex items-center font-primary text-base pb-3">
+                        <Icon icon="logos:cypress-icon" className="w-8 h-8 mr-2"/>
+                        Cypress
+                    </span>
+                        
                     <span className="flex items-center font-primary text-base pb-3">
                         <Icon icon="skill-icons:jest" className="w-8 h-8 mr-2"/>
                         Jest
@@ -79,30 +101,12 @@ const TechStack = () => {
                         <Icon icon="logos:mocha" className="w-8 h-8 mr-2"/>
                         Mocha
                     </span>
-                </div>
-                
-                <div className="flex flex-col px-6">
-                    <h3 className={header}>Miscellaneous</h3>
-                    
-                    <span className="flex items-center font-primary text-base pb-3">
-                            <Icon icon="logos:cypress-icon" className="w-8 h-8 mr-2"/>
-                        Cypress
-                    </span>
-                    
-                    <span className="flex items-center font-primary text-base pb-3">
-                            <Icon icon="skill-icons:mongodb" className="w-8 h-8 mr-2"/>
-                        MongoDB
-                    </span>
-
-                    <span className="flex items-center font-primary text-base pb-3">
-                        <Icon icon="skill-icons:postgresql-light" className="w-8 h-8 mr-2"/>
-                        PostgreSQL
-                    </span>
 
                     <span className="flex items-center font-primary text-base pb-3">
                         <Icon icon="skill-icons:git" className="w-8 h-8 mr-2"/>
                         Git
                     </span>
+                    
                 </div>
                 </div>
                 <button onClick={scrollLeft} className="px-16 text-center font-primary my-8">{"<"} BACK </button>
