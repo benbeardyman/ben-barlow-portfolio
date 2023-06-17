7c7985579283dafac806fe0338112070e5b19c6d
Ben Barlow <ben.barlow@yahoo.co.uk>
2023-06-17 10:37:35 +0100

    nav-arrow fixed

--- src/components/Title.js
+++ src/components/Title.js
@@ -10,7 +10,7 @@ const Title = () => {
     
 
     return (
-        <div id="title" className="flex flex-col items-center justify-center min-h-screen">
+        <div id="title" className="flex flex-col items-center justify-center min-h-screen relative">
             <div className="flex">
                 <h1 className="text-coral text-8xl text-center font-title slide-top mr-4" >
                     Ben
@@ -22,14 +22,14 @@ const Title = () => {
             <h2 className="text-coral text-4xl text-center font-title animate-fade">
                 Software Developer
             </h2>
-            <div className="absolute bottom-0 mb-8 " >
+            <div className="absolute bottom-0 mb-8" >
             <Icon
                     icon="simple-line-icons:arrow-down"
                     onClick={scrollDown}
                     color="#ff9c99"
                     className="h-12 w-12 opacity-70 cursor-pointer hover:opacity-100 animate-fade" 
                 />
-                </div>
+            </div>
         </div>
     )
 }
