45beea15849cd0b02f1721cb27b5b78420fbd8e6
Ben Barlow <ben.barlow@yahoo.co.uk>
2023-07-05 11:51:46 +0100

    scroll experiment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,62 +1,81 @@
-import { Icon } from '@iconify/react'
+import { Icon } from "@iconify/react";
 
-
-const ProjectItem = ({ name, description, image, url, technologies, index}) => {
-    
-    const technologiesArray = technologies.map((technology, index) => {
-        if (technology.iconName === 'firebase') {
-            return (
-                <div className='group flex flex-col justify-center items-center'>
-                    <Icon
-                        key={index}
-                        icon={'logos:firebase'}
-                        className='w-8 h-8 md:w-12 md:h-12 mx-1 md:mx-2 mt-2'
-                    />
-                    <span className='opacity-0 group-hover:opacity-100 absolute bg-white text-black text-sm py-1 px-2 rounded-md font-primary'>
-                        {technology.name}
-                    </span>
-                </div>
-            )
-        } else {
-            return (
-                <div className='group flex flex-col justify-center items-center'>
-                    <Icon
-                        key={index}
-                        icon={'skill-icons:' + technology.iconName}
-                        className='w-8 h-8 md:w-12 md:h-12 mx-1 md:mx-2 mt-2'
-                    />
-                    <span className='opacity-0 group-hover:opacity-100 absolute bg-white text-black text-sm py-1 px-2 rounded-md font-primary'>
-                        {technology.name}
-                    </span>
-                </div>
-            );
-        }
-    })
-
-    return (
-        <div className='flex justify-center w-screen snap-center shrink-0'>
-            <div className='flex flex-col w-10/12 lg:w-3/4 bg-clotted-cream border-2 md:border-4 border-coral rounded-xl'>
-                <img src={process.env.PUBLIC_URL + "/" + image} alt='project specific' className='self-center border-2 border-gray-500 w-11/12 lg:w-3/5 m-4 rounded-lg'/>
-                <div className='flex flex-col justify-center items-center'>
-                    <div className='flex flex-row items-center justify-center'>
-                        <h3 className='px-4 pt-2 md:pt-4 pb-4 md:pb-6 font-header text-lg md:text-xl'>{name}</h3>
-                        <a href={url} target="_blank" rel="noreferrer" className='pb-2 md:pb-4'>
-                            <Icon
-                            icon="bi:github"
-                            className='h-8 w-8 md:h-10 md:w-10'
-                            />
-                        </a>
-                    </div>
-                    <p className='px-4 md:px-16 font-primary text-sm md:text-base'>{description}</p>
-                    <h4 className='px-4 pt-4 m-0 font-header md:text-lg'>Built With:</h4>
-                    <div className='flex flex-wrap mb-4'>{technologiesArray}</div>    
-                </div>
-                
-                
-            </div>
+const ProjectItem = ({
+  name,
+  description,
+  image,
+  url,
+  technologies,
+  index,
+  currentIndex,
+}) => {
+  const technologiesArray = technologies.map((technology, index) => {
+    if (technology.iconName === "firebase") {
+      return (
+        <div className="group flex flex-col items-center justify-center">
+          <Icon
+            key={index}
+            icon={"logos:firebase"}
+            className="mx-1 mt-2 h-8 w-8 md:mx-2 md:h-12 md:w-12"
+          />
+          <span className="absolute rounded-md bg-white px-2 py-1 font-primary text-sm text-black opacity-0 group-hover:opacity-100">
+            {technology.name}
+          </span>
         </div>
-    )
-    
-}
+      );
+    } else {
+      return (
+        <div className="group flex flex-col items-center justify-center">
+          <Icon
+            key={index}
+            icon={"skill-icons:" + technology.iconName}
+            className="mx-1 mt-2 h-8 w-8 md:mx-2 md:h-12 md:w-12"
+          />
+          <span className="absolute rounded-md bg-white px-2 py-1 font-primary text-sm text-black opacity-0 group-hover:opacity-100">
+            {technology.name}
+          </span>
+        </div>
+      );
+    }
+  });
 
-export default ProjectItem
+  const isActive = index === currentIndex;
+
+  return (
+    <div
+      className={`flex w-screen shrink-0 snap-center justify-center ${
+        isActive ? "active" : ""
+      }`}
+    >
+      <div className="flex w-10/12 flex-col rounded-xl border-2 border-coral bg-clotted-cream md:border-4 lg:w-3/4">
+        <img
+          src={process.env.PUBLIC_URL + "/" + image}
+          alt="project specific"
+          className="m-4 w-11/12 self-center rounded-lg border-2 border-gray-500 lg:w-3/5"
+        />
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex flex-row items-center justify-center">
+            <h3 className="px-4 pb-4 pt-2 font-header text-lg md:pb-6 md:pt-4 md:text-xl">
+              {name}
+            </h3>
+            <a
+              href={url}
+              target="_blank"
+              rel="noreferrer"
+              className="pb-2 md:pb-4"
+            >
+              <Icon icon="bi:github" className="h-8 w-8 md:h-10 md:w-10" />
+            </a>
+          </div>
+          <p className="px-4 font-primary text-sm md:px-16 md:text-base">
+            {description}
+          </p>
+          <h4 className="m-0 px-4 pt-4 font-header md:text-lg">Built With:</h4>
+          <div className="mb-4 flex flex-wrap">{technologiesArray}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectItem;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,73 +1,87 @@
-import ProjectItem from "./ProjectItem"
-import { Icon } from '@iconify/react'
+import { useState } from "react";
+import ProjectItem from "./ProjectItem";
+import { Icon } from "@iconify/react";
 
 const Projects = ({ projects }) => {
+  const [currentProjectIndex, setCurrentProjectIndex] = useState(0);
+  const scrollDown = () => {
+    const contactComponent = document.getElementById("contact");
+    contactComponent.scrollIntoView({ behavior: "smooth" });
+  };
 
-    const scrollDown = () => {
-        const contactComponent = document.getElementById('contact');
-        contactComponent.scrollIntoView({ behavior: 'smooth' });
-    };
+  // const scrollLeft = () => {
+  //     const slider = document.getElementById('projects-slider')
+  //     //need to make dynamic - scrollTo would be better
+  //     slider.scrollLeft = slider.scrollLeft -1000
+  // }
 
-    const scrollLeft = () => {
-        const slider = document.getElementById('projects-slider')
-        //need to make dynamic - scrollTo would be better
-        slider.scrollLeft = slider.scrollLeft -1000
-    }
+  // const scrollRight = () => {
+  //     const slider = document.getElementById('projects-slider')
+  //      //need to make dynamic - scrollTo would be better
+  //     slider.scrollLeft = slider.scrollLeft +1000
+  // }
 
-    const scrollRight = () => {
-        const slider = document.getElementById('projects-slider')
-         //need to make dynamic - scrollTo would be better
-        slider.scrollLeft = slider.scrollLeft +1000
-    }
+  const scrollLeft = () => {
+    setCurrentProjectIndex((prevIndex) => Math.max(prevIndex - 1, 0));
+  };
 
-    const projectsArray = projects.map((project, index) => {
-        return (
-            <ProjectItem
-                key={project.id}
-                index={index}
-                name={project.name}
-                description={project.description}
-                image={project.img}
-                url={project.url} 
-                technologies={project.technologies}
-                
-            />
-            
-        )
-    })
+  const scrollRight = () => {
+    setCurrentProjectIndex((prevIndex) =>
+      Math.min(prevIndex + 1, projects.length - 1)
+    );
+  };
 
-    
+  const projectsArray = projects.map((project, index) => {
     return (
-        <div id='projects' className="flex flex-col min-h-screen justify-center relative">
-            <div className="flex items-center">
-                <Icon
-                    icon="bxs:left-arrow"
-                    onClick={scrollLeft}
-                    color="#ff9c99"
-                    className='h-8 w-8 md:h-12 md:w-12 ml-1 md:ml-4 lg:ml-16 opacity-50 cursor-pointer hover:opacity-100' 
-                />
-                
-                <div id="projects-slider" className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory scroll-smooth no-scrollbar'>
-                    {projectsArray}
-                </div>
-                <Icon
-                    icon="bxs:right-arrow"
-                    onClick={scrollRight}
-                    color="#ff9c99"
-                    className='h-8 w-8 md:h-12 md:w-12 mr-1 md:mr-4 lg:mr-16 opacity-50 cursor-pointer hover:opacity-100' 
-                />
-                <div className='absolute bottom-0 mb-8 left-0 right-0 flex justify-center'>
-                    <Icon
-                        icon="simple-line-icons:arrow-down"
-                        onClick={scrollDown}
-                        color="#ff9c99"
-                        className="h-10 w-10 md:h-12 md:w-12 opacity-70 cursor-pointer hover:opacity-100 animate-fade" 
-                    />
-                </div>
-            </div>
-        </div>    
-    )
+      <ProjectItem
+        key={project.id}
+        index={index}
+        name={project.name}
+        description={project.description}
+        image={project.img}
+        url={project.url}
+        technologies={project.technologies}
+        currentIndex={currentProjectIndex}
+      />
+    );
+  });
 
-}
+  return (
+    <div
+      id="projects"
+      className="relative flex min-h-screen flex-col justify-center"
+    >
+      <div className="flex items-center">
+        <Icon
+          icon="bxs:left-arrow"
+          onClick={scrollLeft}
+          color="#ff9c99"
+          className="ml-1 h-8 w-8 cursor-pointer opacity-50 hover:opacity-100 md:ml-4 md:h-12 md:w-12 lg:ml-16"
+        />
 
-export default Projects
+        <div
+          id="projects-slider"
+          className="no-scrollbar relative flex w-full snap-x snap-mandatory overflow-y-hidden overflow-x-scroll scroll-smooth"
+        >
+          {projectsArray[currentProjectIndex]}
+        </div>
+        <Icon
+          icon="bxs:right-arrow"
+          onClick={scrollRight}
+          color="#ff9c99"
+          className="mr-1 h-8 w-8 cursor-pointer opacity-50 hover:opacity-100 md:mr-4 md:h-12 md:w-12 lg:mr-16"
+        />
+        <div className="absolute bottom-0 left-0 right-0 mb-8 flex justify-center">
+          <Icon
+            icon="simple-line-icons:arrow-down"
+            onClick={scrollDown}
+            color="#ff9c99"
+            className="h-10 w-10 animate-fade cursor-pointer opacity-70 hover:opacity-100 md:h-12 md:w-12"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
